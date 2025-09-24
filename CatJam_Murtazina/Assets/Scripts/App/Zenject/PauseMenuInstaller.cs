543196b9f62c32b0f0a8e0ca633ee0d46443e429
Beshamon <nastya.murtazina.00@inbox.ru>
2025-09-24 20:20:40 +0300

    Refactoring pause menu

--- CatJam_Murtazina/Assets/Scripts/App/Zenject/PauseMenuInstaller.cs
+++ CatJam_Murtazina/Assets/Scripts/App/Zenject/PauseMenuInstaller.cs
@@ -7,22 +7,14 @@ namespace CatJam
 {
     public class PauseMenuInstaller : MonoInstaller
     {
-        [SerializeField] private PauseMenuView view;
+        [SerializeField] private PauseMenuView _view;
 
         public override void InstallBindings()
         {
+            Container.Bind<PauseMenuView>().FromInstance(_view).AsSingle();
             Container.Bind<PauseMenuModel>().AsSingle();
-            Container.Bind<PauseMenuView>().FromInstance(view).AsSingle();
-            Container.Bind<PauseMenuController>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
-        }
-
-        private void Start()
-        {
-            var controller = Container.Resolve<PauseMenuController>();
-            var model = Container.Resolve<PauseMenuModel>();
-            var viewInstance = Container.Resolve<PauseMenuView>();
-
-            controller.Initialize(viewInstance, model);
+            Container.Bind<IViewAnimator>().To<FadeViewAnimator>().AsSingle();
+            Container.BindInterfacesAndSelfTo<PauseMenuController>().AsSingle();
         }
     }
 }
--- CatJam_Murtazina/Assets/Scripts/UI/PauseMenu/PauseMenuController.cs
+++ CatJam_Murtazina/Assets/Scripts/UI/PauseMenu/PauseMenuController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System;
 using Models.PauseMenuModel;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,29 +6,43 @@ using Zenject;
 
 namespace CatJam.PauseMenu
 {
-    public class PauseMenuController
+    public class PauseMenuController : IInitializable, IDisposable
     {
-        [Inject] private ISoundService _soundService;
-        [Inject] private Timer _timer;
-        [Inject] private IViewAnimator _viewAnimator;
+        private readonly ISoundService _soundService;
+        private readonly IViewAnimator _viewAnimator;
+        private readonly PauseMenuModel _model;
+        private readonly PauseMenuView _view;
 
-        private PauseMenuModel _model;
-        private PauseMenuView _view;
-
-        public void Initialize(PauseMenuView view, PauseMenuModel model)
+        public PauseMenuController(ISoundService soundService, IViewAnimator viewAnimator, PauseMenuModel model, PauseMenuView view)
         {
-            _view = view;
+            _soundService = soundService;
+            _viewAnimator = viewAnimator;
             _model = model;
+            _view = view;
+        }
+
+        public void Initialize()
+        {
             _view.Initialize(_viewAnimator);
             _view.OnResumeClicked += HandleResume;
             _view.OnSoundToggleClicked += HandleSoundToggle;
             _view.OnExitClicked += HandleExit;
-
             _model.OnSoundStateChanged += _view.UpdateSoundIcon;
+
+            _view.Show(false);
+        }
+        
+        public void Dispose()
+        {
+            _view.OnResumeClicked -= HandleResume;
+            _view.OnSoundToggleClicked -= HandleSoundToggle;
+            _view.OnExitClicked -= HandleExit;
+            _model.OnSoundStateChanged -= _view.UpdateSoundIcon;
         }
 
         public void PauseGame()
         {
+            if (_model.IsPaused) return;
             _model.Pause();
             Time.timeScale = 0f;
             _view.Show(true);
@@ -36,16 +50,11 @@ namespace CatJam.PauseMenu
 
         private void HandleResume()
         {
+            
+            if (!_model.IsPaused) return;
             _view.Show(false);
             _model.Resume();
             Time.timeScale = 1f;
-            //StartCoroutine(ResumeAfterDelay());
-        }
-
-        private IEnumerator ResumeAfterDelay()
-        {
-            yield return new WaitForSeconds(3f);
-            _timer.ResumeTimer();
         }
 
         private void HandleSoundToggle()
