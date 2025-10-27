34a0f9de7b1d1a0421dd1ed503f8c539d5fa4296
Beshamon <nastya.murtazina.00@inbox.ru>
2025-10-27 23:00:47 +0300

    Add breathing background main menu

---
+++ CatJam_Murtazina/Assets/Scripts/Gameplay/Menu/BackgroundBreathing.cs
@@ -0,0 +1,27 @@
+﻿namespace CatJam.Menu
+{
+    using UnityEngine;
+    using DG.Tweening;
+
+    public class BackgroundBreathing : MonoBehaviour
+    {
+        [SerializeField] private float zoomInScale = 1.1f;
+        [SerializeField] private float duration = 4f;
+        [SerializeField] private Ease easeType = Ease.InOutSine;
+
+        private Tween _zoomTween;
+
+        private void Start()
+        {
+            _zoomTween = transform
+                .DOScale(zoomInScale, duration / 2f)
+                .SetEase(easeType)
+                .SetLoops(-1, LoopType.Yoyo); // бесконечный цикл туда-сюда
+        }
+
+        private void OnDestroy()
+        {
+            _zoomTween?.Kill();
+        }
+    }
+}
