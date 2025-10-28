5ca36ebe2f92b8519fa5aab5e3eae7126554eccf
Beshamon <nastya.murtazina.00@inbox.ru>
2025-10-28 23:05:05 +0300

    Change main menu

--- CatJam_Murtazina/Assets/Scripts/UI/StartGameButton/StartGameButtonView.cs
+++ CatJam_Murtazina/Assets/Scripts/UI/StartGameButton/StartGameButtonView.cs
@@ -1,16 +1,22 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
 using Models.MainMenu;
+using UnityEngine.EventSystems;
 
 namespace UI.StartGameButton
 {
     [RequireComponent(typeof(Button))]
-    public class StartGameButtonView : MonoBehaviour
+    public class StartGameButtonView : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] private Button _button;
+        [SerializeField] private float _hoverScale  = 1.1f;
+        [SerializeField] private float _hoverDuration  = 0.2f;
 
         private SignalBus _signalBus;
+        private Vector3 _originalScale;
+        private Tween _scaleTween;
 
         [Inject]
         private void Construct(SignalBus signalBus)
@@ -28,17 +34,35 @@ namespace UI.StartGameButton
             if (_button == null) 
                 _button = GetComponent<Button>();
             
+            _originalScale = _button.transform.localScale;
             _button.onClick.AddListener(OnClick);
         }
 
         private void OnDestroy()
         {
             _button.onClick.RemoveListener(OnClick);
+            _scaleTween?.Kill();
         }
 
         private void OnClick()
         {
             _signalBus.Fire<StartGameSignal>();
+        }
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _scaleTween?.Kill();
+            _scaleTween = _button.transform
+                .DOScale(_originalScale * _hoverScale, _hoverDuration)
+                .SetEase(Ease.OutBack);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _scaleTween?.Kill();
+            _scaleTween = _button.transform
+                .DOScale(_originalScale, _hoverDuration)
+                .SetEase(Ease.InOutSine);
         }
     }
 }
